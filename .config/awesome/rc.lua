4b7d62ff4c2d276641038d2e1404bbfb10e886f2
xcession <xcession@gmail.com>
2008-12-20 22:44:56 +0700

    fix awesome rc theme

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -14,42 +14,13 @@ require("naughty")
 --------------------------------------------------------------------------------
 --{{{ Theme!
 
-beautiful.font                = "Terminus 8"
-
-beautiful.bg_normal           = "#000000AA"
-beautiful.fg_normal           = "#EFEFEF"
-
-beautiful.bg_focus            = "#000000"
-beautiful.fg_focus            = "#FF0033"
-
-beautiful.bg_urgent           = "#FF0033AA"
-beautiful.fg_urgent           = "#EFEFEF"
-
-beautiful.border_width        = "1"
-beautiful.border_normal       = "#4C4C4C66"
-beautiful.border_focus        = "#FF003366"
-beautiful.border_marked       = "#FF000066"
-
-beautiful.taglist_squares_sel       = "/usr/share/awesome/themes/default/taglist/squarefw.png"
-beautiful.taglist_squares_unsel     = "/usr/share/awesome/themes/default/taglist/squarew.png"
-beautiful.tasklist_floating_icon    = "/usr/share/awesome/themes/default/tasklist/floatingw.png"
-beautiful.awesome_icon              = "/usr/share/awesome/icons/awesome16.png"
-
-beautiful.menu_submenu_icon         = awful.util.getdir("config").."/icons/submenu.png"
-beautiful.menu_height               = "16"
-beautiful.menu_width                = "100"
-
-beautiful.taglist_squares = true
-beautiful.titlebar_close_button = true
-
 use_titlebar = false
 
 --}}}
 --------------------------------------------------------------------------------
 --{{{ Register theme (don't change this)
 
-awful.beautiful.register(beautiful)
-awesome.font(beautiful.font)
+beautiful.init(os.getenv("HOME") .. "/.config/awesome/theme)
 
 --}}}
 --------------------------------------------------------------------------------
@@ -122,21 +93,25 @@ end
 
 -- }}}
 --------------------------------------------------------------------------------
--- {{{ Wibox
+--{{{ Menu
+-- Popup menu when we rightclick the desktop
 
--- Create a laucher widget and a main menu
-wi_menu = {
-   { "manual", terminal .. " -e man awesome" },
-   { "restart", awesome.restart },
-   { "quit", awesome.quit }
-}
+-- Submenu
+awesomemenu         = { { "Edit config" , term.." -e vim "..awful.util.getdir("config").."/rc.lua" }
+                      , { "Restart"     , awesome.restart }
+                      , { "Quit"        , awesome.quit }
+                      }
+-- Main menu
+mainmenu            = awful.menu.new({ items = { { "Terminal"    , term }
+                                               , { "Awesome"     , awesomemenu }
+                                               }
+                                     })
 
-wi_mainmenu = awful.menu.new({ items = { { "awesome", wi_menu, beautiful.awesome_icon },
-                                        { "open terminal", terminal }
-                                      }
-                            })
+--}}}
+-------------------------------------------------------------------------------------
+--{{{ Widgets
 
-wi_launcher = awful.widget.launcher({menu = wi_mainmenu })
+wi_launcher = awful.widget.launcher({menu = mainmenu })
 
 -- Create a systray
 wi_systray = widget({ type = "systray", align = "right" })
