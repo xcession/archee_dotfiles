1b8d3417c0d43956ad538f2d0f1b5b6d79bd3042
xcession <xcession@gmail.com>
2008-12-21 01:54:55 +0700

    update battery widget function

--- .config/awesome/functions.lua
+++ .config/awesome/functions.lua
@@ -36,11 +36,11 @@ function batteryInfo(adapter)
     if sta:match("Charging") then
         dir = "^"
         battery = "A/C " .. battery
-        battery = setFg("#3465a4", dir)
+        battery = setFg("#3465a4", battery)
     elseif sta:match("Discharging") then
         dir = "v"
         if tonumber(battery) >= 25 and tonumber(battery) <= 50 then
-            battery = setFg("#ffc123", dir)
+            battery = setFg("#ffc123", battery)
         elseif tonumber(battery) < 25 then
             if tonumber(battery) <= 10 then
                 naughty.notify({
@@ -52,17 +52,17 @@ function batteryInfo(adapter)
                                     bg       = beautiful.bg_focus
                                })
             end
-            battery = setFg("#ff6565", dir)
+            battery = setFg("#ff6565", battery)
         else
             battery = battery
         end
     else
         dir = "="
         battery = "A/C"
-        battery = setFg("#93d44f", dir)
+        battery = setFg("#93d44f", battery)
     end
     
-    batterywidget.text = " Battery: " .. dir .. battery .. "%" .. dir .. " "
+    batterywidget.text = " Battery: " .. dir .. battery .. dir .. " "
 end
 
 --}}}
